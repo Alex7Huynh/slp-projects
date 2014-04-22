c2aa636d210e9fecd46bf3d2ae8838f8c8a91c9f
masterminh219@gmail.com <masterminh219@gmail.com@9cbeec0c-bc46-392e-d32b-b7aff9adc736>
2014-04-22 10:56:11 +0000

    Configure custom number of Loop and number of Gauss

--- Lab1/Project_lab1/AudioGMM.m
+++ Lab1/Project_lab1/AudioGMM.m
@@ -13,13 +13,13 @@ classdef AudioGMM
     end
     
     methods
-        function obj = AudioGMM(pathFile, label)
+        function obj = AudioGMM(pathFile, label, numLoop, numGauss)
             obj.label = label;
             obj.audio = wavread(pathFile);
             obj.audio = mfcc(obj.audio);
-            g0 = gNew(12, AudioGMM.numberOfGauss, 'diag');
-            g1 = gInit(g0,obj.audio, AudioGMM.numberOfLoop);
-            obj.g2 = gRE(g1,obj.audio, AudioGMM.numberOfLoop);
+            g0 = gNew(12, numGauss, 'diag');
+            g1 = gInit(g0,obj.audio, numLoop);
+            obj.g2 = gRE(g1,obj.audio, numLoop);
         end
         
         function x = calculate(obj,pathSample)
